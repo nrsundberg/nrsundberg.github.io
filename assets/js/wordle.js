4a0b99890b4538f1772da0c1a52904a97ec5a372
nrsundberg <nrsundbe@ncsu.edu>
2022-11-04 09:17:50 -0400

    on-screen keyboard

--- assets/js/wordle.js
+++ assets/js/wordle.js
@@ -74,6 +74,34 @@ document.addEventListener("keyup", function(evt) {
         return;
     }
 });
+function screenKeyboardInput(letter) {
+    if (gameWon == 'yes'){
+        return
+    }
+    const typedLetter = letter.toUpperCase()
+    const pressedKey = letter
+    gameBoardRows[guessAttempt].classList.remove("apply-shake");    
+    if (pressedKey == `Backspace`) {
+        if (guess.length === 0) {
+            return;
+        }
+        fillBoardLetter(``);
+        guess = guess.slice(0, -1);
+    } else if (guess.length === 5 && pressedKey != `Enter`) {
+        return;
+    } else if (pressedKey != `Enter` && pressedKey !=  `Backspace`) {
+        guess += typedLetter;
+        fillBoardLetter(typedLetter)
+    } else if (pressedKey === `Enter`) {
+        if (guessAttempt === 0){
+            answer = newAnswer(answers);
+            answerLetters = answer.split("");
+        }
+        checkForValidGuess(guess);
+    } else {
+        return;
+    }
+};
 function checkForValidGuess(guess) {
     if (words.includes(guess.toLowerCase())) {
         guessLetterInformation(guess);
@@ -130,15 +158,143 @@ function countOccurances(letter, arrayOfLetters) {
     }
     return freq;
 }
-// document.addEventListener('readystatechange', event => { 
-//     // When window loaded ( external resources are loaded too- `css`,`src`, etc...) 
-//     if (event.target.readyState === "complete") {
-//         console.log(`done loading`);
-//         console.log(answers);
-//         // newGame();
-//         const newGameButton = document.getElementById("new-game-btn");
-//         newGameButton.addEventListener("click", newGame);
-//     }
-// });
 const newGameButton = document.getElementById("new-game-btn");
 newGameButton.addEventListener("click", newGame);
+
+// keyboard
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: "",
+        capsLock: false
+    },
+
+    init() {
+        // Create main elements
+        this.elements.main = document.createElement("div");
+        this.elements.keysContainer = document.createElement("div");
+
+        // Setup main elements
+        this.elements.main.classList.add("keyboard");
+        this.elements.keysContainer.classList.add("keyboard__keys");
+        this.elements.keysContainer.appendChild(this._createKeys());
+
+        this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
+
+        // Add to DOM
+        this.elements.main.appendChild(this.elements.keysContainer);
+        document.body.appendChild(this.elements.main);
+
+        // Automatically use keyboard for elements with .use-keyboard-input
+        document.querySelectorAll(".use-keyboard-input").forEach(element => {
+            element.addEventListener("focus", () => {
+                this.open(element.value, currentValue => {
+                    element.value = currentValue;
+                });
+            });
+        });
+    },
+
+    _createKeys() {
+        const fragment = document.createDocumentFragment();
+        const keyLayout = [
+            
+              "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+            "a", "s", "d", "f", "g", "h", "j", "k", "l", "Backspace",
+                  "z", "x", "c", "v", "b", "n", "m","Enter",
+        ];
+
+        // Creates HTML for an icon
+        const createIconHTML = (icon_name) => {
+            return `<i class="material-icons">${icon_name}</i>`;
+        };
+
+        keyLayout.forEach(key => {
+            const keyElement = document.createElement("button");
+            const insertLineBreak = ["p", "Backspace", "Enter"].indexOf(key) !== -1;
+
+            // Add attributes/classes
+            keyElement.setAttribute("type", "button");
+            keyElement.classList.add("keyboard__key");
+
+            switch (key) {
+                case "Backspace":
+                    keyElement.classList.add("keyboard__key--wide");
+                    keyElement.innerHTML = createIconHTML("Backspace");
+
+                    keyElement.addEventListener("click", () => {
+                        // this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+                        this.properties.value = "Backspace";
+                        screenKeyboardInput(this.properties.value);
+                        this._triggerEvent("oninput");
+                    });
+
+                    break;
+
+                case "Enter":
+                    keyElement.classList.add("keyboard__key--wide");
+                    keyElement.innerHTML = createIconHTML("Enter");
+
+                    keyElement.addEventListener("click", () => {
+                        this.properties.value = "Enter";
+                        screenKeyboardInput(letter = this.properties.value);    
+                        this._triggerEvent("oninput");
+                    });
+
+                    break;
+
+                default:
+                    keyElement.textContent = key.toLowerCase();
+
+                    keyElement.addEventListener("click", () => {
+                        this.properties.value = this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+                        console.log(this.properties.value)
+                        screenKeyboardInput(letter = (this.properties.value));
+                        this._triggerEvent("oninput");
+                    });
+
+                    break;
+            }
+
+            fragment.appendChild(keyElement);
+
+            if (insertLineBreak) {
+                fragment.appendChild(document.createElement("br"));
+            }
+        });
+
+        return fragment;
+    },
+
+    _triggerEvent(handlerName) {
+        if (typeof this.eventHandlers[handlerName] == "function") {
+            this.eventHandlers[handlerName](this.properties.value);
+        }
+    },
+
+    open(initialValue, oninput, onclose) {
+        this.properties.value = initialValue || "";
+        this.eventHandlers.oninput = oninput;
+        this.eventHandlers.onclose = onclose;
+    },
+
+    close() {
+        this.properties.value = "";
+        this.eventHandlers.oninput = oninput;
+        this.eventHandlers.onclose = onclose;
+    }
+};
+
+window.addEventListener("DOMContentLoaded", function () {
+    Keyboard.init();
+});
